4d4bff74cedf6c9211197a3c749ae6fe1325e2a2
Rrch <rrch.cr8@gmail.com>
2021-06-06 01:16:29 -0400

    Add project files.

---
+++ ContosoRecipes/Controllers/RecipesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoRecipes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+        [HttpGet]
+        public string[] GetDishes()
+        {
+            string[] dishes = { "Oxtail", "Curry Chicken", "Dumplings" };
+            return dishes;
+
+        }
+    }
+}
